0503dc11173bcd1db8ea7c817e818767faf30642
Abinesh <157296087+abinesh545@users.noreply.github.com>
2026-01-13 09:51:09 +0530

    Fix error message for division by zero test

--- test_main.py
+++ test_main.py
@@ -13,7 +13,7 @@ def test_mul():
 def test_div():
     assert division(54,9)==6
     assert division(72,4)==18
-    with pytest.raises(ValueError, match="Cannot Divide by zero"):
+    with pytest.raises(ValueError, match="Cannot Divide by Zero"):
         division(32, 0)
 def test_mod():
     assert modulo(45,4)==1

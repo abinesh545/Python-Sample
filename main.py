17340abc50590e94b22e41c526289547073995fc
Abinesh <157296087+abinesh545@users.noreply.github.com>
2026-01-13 09:23:27 +0530

    Add multiplication, division, and modulo functions

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ def addition(a,b):
     return a+b
 def subtraction(a,b):
     return a-b
-
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a//b
+def modulo(a,b):
+    return a%b
